641cd6028a79d25bc7b3d3804f8e395cc4572fbf
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-06-18 15:27:10 +0545

    chore: created an ui button

---
+++ src/ui/common/atom/Button.tsx
@@ -0,0 +1,15 @@
+
+interface ButtonProps {
+  name: string,
+  type: "submit" | "reset" | "button" | undefined
+}
+
+const Button: React.FC<ButtonProps> = ({ name, type = 'submit' }) => {
+  return (
+    <div>
+      <button type={type}>{name}</button>
+    </div>
+  )
+}
+
+export default Button
