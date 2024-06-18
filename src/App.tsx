86ce78d9540ac28c4c5610c0e475995a25a4f25b
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-06-18 15:24:35 +0545

    chore: created browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,43 @@
-import './App.css'
+import '@assets/sass/style.scss';
+import Login from '@ui/landingPage/organisms/Login';
+import Register from '@ui/landingPage/organisms/Register';
+import LandingTemplate from '@ui/landingPage/templates/LandingTemplate';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import './App.css';
+import LandingPage from '@ui/landingPage/pages/LandingPage';
+
 
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <LandingTemplate />,
+      children: [
+        {
+          path: "",
+          element: <LandingPage />,
+          index: true
+        },
+        {
+          path: "login",
+          element: <Login />,
+          index: true
+        },
+        {
+          path: "register",
+          element: <Register />,
+          index: true
+        },
+
+      ]
+    },
+
+
+  ]);
+
   return (
-    <>
-      
-    </>
+    <RouterProvider router={router} />
   )
 }
 
