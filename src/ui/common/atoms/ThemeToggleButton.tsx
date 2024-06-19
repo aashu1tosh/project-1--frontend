4493f334fd2c64c40627c4c8393a2499a05ed819
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-06-19 14:08:28 +0545

    chore made some design changes

---
+++ src/ui/common/atoms/ThemeToggleButton.tsx
@@ -0,0 +1,12 @@
+// import './ThemeToggleButton.css'
+const ThemeToggleButton = () => {
+    return (
+        <label className="rocker rocker-small">
+            <input type="checkbox" />
+            <span className="switch-left"></span>
+            <span className="switch-right"></span>
+        </label>
+    )
+}
+
+export default ThemeToggleButton
