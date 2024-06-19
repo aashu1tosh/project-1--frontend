c4886b962c518c1cf664e479a04d4632f593a728
Aashutosh Parajuli <aashutosh282@gmail.com>
2024-06-19 17:27:42 +0545

    feat: multilanguage localization done

---
+++ src/data/localization/landingPage/login.ts
@@ -0,0 +1,32 @@
+import { LabelType } from '@type/global.types'
+
+export const loginPageLabel: LabelType = {
+    welcomeTo: {
+        en: 'Welcome to',
+        ne: 'स्वागत छ',
+    },
+    enterCredentials: {
+        en: 'Enter your credentials',
+        ne: 'आफ्नो प्रमाणहरू प्रविष्ट गर्नुहोस्',
+    },
+    forgotPassword: {
+        en: 'Forgot Password',
+        ne: 'पासवर्ड बिर्सिएँ'
+    },
+    registerNow: {
+        en: "Don't have an account! Register now.",
+        ne: "खाता छैन! अहिले नै दर्ता गर्नुहोस।"
+    },
+    email: {
+        en: "Email",
+        ne: "इमेल"
+    },
+    password: {
+        en: "Password",
+        ne: "पासवर्ड"
+    },
+    login: {
+        en: 'Login',
+        ne: 'लग-इन',
+    }
+}
